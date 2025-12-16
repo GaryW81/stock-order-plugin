8cbff8bdc85aa73409f2a9e8bbb5f07014e942dd
GaryW81 <gary@wilson-organisation.com>
2025-12-16 22:09:12 +0000

    V6.2.2 - Resize image

--- admin/preorder-ui.php
+++ admin/preorder-ui.php
@@ -128,7 +128,7 @@ function sop_preorder_render_admin_page() {
     if ( ! current_user_can( 'manage_woocommerce' ) ) {
         wp_die( esc_html__( 'You do not have permission to access this page.', 'sop' ) );
     }
-
+ 
     $suppliers = sop_preorder_get_suppliers();
     $settings  = sop_preorder_get_settings();
 
